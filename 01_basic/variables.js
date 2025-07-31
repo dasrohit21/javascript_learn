c666887ad16919dd0219074c810e5a6c5c2e5d0a
ROHIT <76683139+dasrohit21@users.noreply.github.com>
2025-07-31 16:03:41 +0000

    variables

---
+++ 01_basic/variables.js
@@ -0,0 +1,18 @@
+const accountId = 144543
+let accountemail = "rohit@1234"
+var accountpass = "12345"
+accountcity = "jaipur"
+
+let accountstate;
+
+//accountId = 2 not allowed
+
+accountemail = "hcgf@gamil.com"
+accountpass = "2121122"
+accountcity = "bengal"
+
+console.log(accountId);
+
+console.table([accountId,accountemail,accountpass,accountcity,accountstate]);
+
+
