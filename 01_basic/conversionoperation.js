43c51c35a77f1197c47cfca820df0614fb101bc4
ROHIT <76683139+dasrohit21@users.noreply.github.com>
2025-07-31 16:57:51 +0000

    operation

--- 01_basic/conversionoperation.js
+++ 01_basic/conversionoperation.js
@@ -31,7 +31,7 @@ let stringNumber = String(someNumber)
 
 let value = 3
 let negValue = -value
-// console.log(negValue);
+//console.log(negValue)
 
 // console.log(2+2);
 // console.log(2-2);
@@ -41,7 +41,7 @@ let negValue = -value
 // console.log(2%3);
 
 let str1 = "hello"
-let str2 = " hitesh"
+let str2 = " rohit"
 
 let str3 = str1 + str2
 // console.log(str3);
@@ -50,6 +50,7 @@ let str3 = str1 + str2
 // console.log(1 + "2");
 // console.log("1" + 2 + 2);
 // console.log(1 + 2 + "2");
+// console.log(1 + "2" + 2);
 
 // console.log( (3 + 4) * 5 % 3);
 
@@ -58,8 +59,10 @@ let str3 = str1 + str2
 
 let num1, num2, num3
 
-num1 = num2 = num3 = 2 + 2
+num1 = num2 = num3 = 2 + 2 //not prefered
 
 let gameCounter = 100
-++gameCounter;
-//console.log(gameCounter);
+
+console.log(gameCounter);
+gameCounter++;
+console.log(gameCounter);
