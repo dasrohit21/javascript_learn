c877f9f68fab3021cd5777dfc8b8568138aea281
ROHIT <76683139+dasrohit21@users.noreply.github.com>
2025-08-07 17:11:53 +0000

    arrow

--- 03_basic/arrow.js
+++ 03_basic/arrow.js
@@ -8,3 +8,43 @@ const user = {
     }
 
 }
+
+// user.welcomeMessage()
+// user.username ='rohit'
+// user.welcomeMessage()
+
+// console.log(this);
+
+// function chai(){
+//     let username ="devraj"
+//     console.log(this.username);
+// }
+
+// chai()
+
+// const chai = function () {
+//     let username ="devraj"
+//     console.log(this.username);
+// }
+
+// chai()
+
+// const chai = () => {
+//     let username ="devraj"
+//     console.log(this);
+// }
+
+// chai()
+
+// const addTwo = (num1, num2) =>  num1 + num2
+
+// const addTwo = (num1, num2) =>  (num1 + num2)
+
+const addTwo = (num1, num2) =>  ({username:"rohit"})
+
+
+console.log(addTwo(3, 4))
+
+const myArray = [2, 5, 3, 7, 8]
+
+myArray.forEach()
